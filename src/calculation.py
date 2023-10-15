7812fa6ce536029a8829c091a2d7c0f0f30afef6
Tossapol Maithong <mozzarellala9@gmail.com>
2023-10-15 20:26:50 +0700

    update

---
+++ src/calculation.py
@@ -0,0 +1,13 @@
+def calculation_minus(digit_1, digit_2):
+    #minus digit
+    result = digit_1-digit_2
+    #print(result)
+    return result
+
+
+def calculation_plus(digit_1, digit_2):
+    #plus digit
+    result = digit_1+digit_2
+    #print(result)
+    return result
+
---
+++ test2.py
@@ -0,0 +1,15 @@
+# %% [markdown]
+# # Markdown
+
+# %% [markdown]
+# Test Markdown
+
+# %%
+print("Hello World")
+
+# %% [markdown]
+# - bullet
+# - bullet2 
+#   - bullet
+
+

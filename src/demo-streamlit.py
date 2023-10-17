417fa2993bea43e57169bfc108dffe6f00a91751
Tossapol-MT <mozzarellala9@gmail.com>
2023-10-17 16:10:39 +0700

    update content

--- src/demo-streamlit.py
+++ src/demo-streamlit.py
@@ -15,14 +15,6 @@ st.subheader("subheader")
 st.write("Hello World")
 st.caption("Caption")
 
-df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
-st.dataframe(df,width=1500)
-
-st.area_chart(df)
-st.line_chart(df)
-st.scatter_chart(df)
-
-
 data = pd.DataFrame(data=np.random.rand(4,4)*100000,
 index=["Q1","Q2","Q3","Q4"],
 columns=["East","West","North","South"])
@@ -62,3 +54,73 @@ st.download_button(
     file_name='df.csv',
     mime='text/csv', 
 )
+
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+st.dataframe(df,width=1500)
+
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("col1")
+    st.area_chart(df)
+
+with col2:
+    st.header("col2")
+    st.line_chart(df)
+
+with col3:
+    st.header("col3")
+    st.scatter_chart(df)
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("col1")
+    st.area_chart(df)
+
+with col2:
+    st.header("col2")
+    st.line_chart(df)
+
+
+with st.expander("See explanation"):
+    st.dataframe(df,width=1500)
+
+contact_list = ["Email","Home Phone","Mobile Phone"]
+
+selectbox_value = st.selectbox("Select contacted",contact_list,index=None,
+                               placeholder="Select contact method")
+st.write(selectbox_value)
+
+
+# with st.form("my_form"):
+with st.sidebar.form("my_form"): # add sidebar
+    multi_value = st.multiselect("Select contacted",
+                    contact_list,
+                    placeholder="Select contact method"
+    )
+
+    submit = st.form_submit_button("SUBMIT")
+    if submit:
+        st.write(multi_value)
+
+tab1, tab2 = st.tabs(["tab1","tab2"])
+
+with tab1:
+    st.header("tab1")
+with tab2:
+    st.header("tab2")
+    with st.form("upload_file"):
+                        uploaded_file = st.file_uploader("Choose a file")
+                        if uploaded_file is not None:
+                            df_upload_file = pd.read_excel(uploaded_file)
+
+
+                        submitted = st.form_submit_button("Submit")
+                        if submitted:
+                            if uploaded_file is not None:
+                                st.dataframe(df_upload_file,width=1000)
+        
